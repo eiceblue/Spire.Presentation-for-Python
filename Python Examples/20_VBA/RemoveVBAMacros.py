d2b0dc21f0a7b77717e7daa5c62e94f8416aa9e3
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2023-09-08 09:33:05 +0800

    Add files via upload

--- Python Examples/20_VBA/RemoveVBAMacros.py
+++ Python Examples/20_VBA/RemoveVBAMacros.py
@@ -1,1 +1,15 @@
+from spire.presentation.common import *
+from spire.presentation import *
 
+inputFile = "./Data/Macros.ppt"
+outputFile = "RemoveVBAMacros.ppt"
+
+presentation = Presentation()
+
+# Load PPT file from disk
+presentation.LoadFromFile(inputFile)
+# Remove macros
+# Note, at present it only can work on macros in PPT file, has not supported for PPTM file yet.
+presentation.DeleteMacros()
+presentation.SaveToFile(outputFile, FileFormat.PPT)
+presentation.Dispose()
